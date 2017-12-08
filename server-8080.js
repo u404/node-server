7bcadc63ed29137b69652ccf90c40fa3cb04b27a
ghzhang <ghzhang@huikedu.com>
2017-12-08 15:19:16 +0800

    第一次提交

---
+++ server-8080.js
@@ -0,0 +1,21 @@
+const Koa = require('Koa');
+const app = new Koa();
+
+const main = ctx => {
+    ctx.response.type = 'json';
+    ctx.response.body = {
+        "result": "ok",
+        "message": "登录成功",
+        "user":{
+            "id": "E6A232B2DEDF69469C33DC5901307461",
+            "name": "学员A",
+            "avatar": "http://domain.com/icon.png",
+            "customua": "customua1",
+            "viewercustommark": "mark1",
+            "marquee": "{\"loop\":-1,\"type\":\"text\",\"text\":{\"content\":\"跑马灯内容\",\"font_size\":20,\"color\":\"0xf0f00f\"},\"action\":[{\"duration\":4000,\"start\":{\"xpos\":0,\"ypos\":0,\"alpha\":0.5},\"end\":{\"xpos\":0.6,\"ypos\":0,\"alpha\":1}},{\"duration\":4000,\"start\":{\"xpos\":0,\"ypos\":0.7,\"alpha\":0.3},\"end\":{\"xpos\":0.7,\"ypos\":0.7,\"alpha\":0.9}}]}"
+        }
+    }
+};
+
+app.use(main);
+app.listen(8080);
